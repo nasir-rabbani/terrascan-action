6e4ca6041667810337cf70dba5519f2eb764d10a
Nasir Rabbani <52099528+nasir-rabbani@users.noreply.github.com>
2021-10-26 14:24:43 +0530

    Update docker-push-latest-tag.sh

--- scripts/docker-push-latest-tag.sh
+++ scripts/docker-push-latest-tag.sh
@@ -10,5 +10,5 @@ LATEST_TAG=$(git describe --abbrev=0 --tags)
 LATEST_TAG_SHORT=$(echo "${LATEST_TAG//v}")
 
 # PS: It is a prerequisite to execute 'docker login' before running this script
-docker tag ${DOCKER_REPO}:${GIT_COMMIT} ${DOCKER_REPO}:${LATEST_TAG_SHORT}
+# docker tag ${DOCKER_REPO}:${GIT_COMMIT} ${DOCKER_REPO}:${LATEST_TAG_SHORT}
 docker push --all-tags
