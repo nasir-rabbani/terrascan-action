d28e457ba6e00432a21509de241485aeefb0f9a4
Nasir Rabbani <nasirrabbani95@gmail.com>
2021-10-26 14:02:51 +0530

    added latest tag for docker image

--- scripts/docker-push-latest-tag.sh
+++ scripts/docker-push-latest-tag.sh
@@ -5,10 +5,10 @@ set -o nounset
 set -o pipefail
 
 GIT_COMMIT=$(git rev-parse --short HEAD 2>/dev/null)
-DOCKER_REPO="accurics/terrascan-action"
+DOCKER_REPO="nasirrabbani/terrascan-action"
 LATEST_TAG=$(git describe --abbrev=0 --tags)
 LATEST_TAG_SHORT=$(echo "${LATEST_TAG//v}")
 
 # PS: It is a prerequisite to execute 'docker login' before running this script
 docker tag ${DOCKER_REPO}:${GIT_COMMIT} ${DOCKER_REPO}:${LATEST_TAG_SHORT}
-docker push ${DOCKER_REPO}:${LATEST_TAG_SHORT}
+docker push --all-tags
